fbac25c2989643c285ad7eb441b92609c8f67921
Julien Gauthier <djgjai@gmail.com>
2025-08-28 19:27:31 +0200

    Delete app directory

--- app/client/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('client', __name__)
-
-from app.client import routes
--- app/main/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, current_app
-from app.main import bp
-
-@bp.route('/')
-@bp.route('/index')
-def index():
-    guidelines = current_app.config['TECHNICAL_GUIDELINES']
-    return render_template('index.html', title='Accueil - n3xtpdf', guidelines=guidelines)
